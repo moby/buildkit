a8303438d0cd8f731036c460905d9e74fa7a7c15
Tonis Tiigi <tonistiigi@gmail.com>
2017-06-15 16:09:03 -0700

    example: more complicated build example
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- examples/llbout/example.go
+++ examples/llbout/example.go
@@ -10,9 +10,12 @@ func main() {
 	busybox := llb.Image("docker.io/library/busybox:latest")
 	mod1 := busybox.Run(llb.Meta{Args: []string{"/bin/sleep", "1"}, Cwd: "/"})
 	mod2 := mod1.Run(llb.Meta{Args: []string{"/bin/sh", "-c", "echo foo > /bar"}, Cwd: "/"})
-	mod3 := mod2.Run(llb.Meta{Args: []string{"/bin/ls", "-l", "/"}, Cwd: "/"})
+	alpine := llb.Image("docker.io/library/alpine:latest")
+	mod3 := mod2.Run(llb.Meta{Args: []string{"/bin/cp", "-a", "/alpine/etc/passwd", "baz"}, Cwd: "/"})
+	mod3.AddMount("/alpine", alpine)
+	mod4 := mod3.Run(llb.Meta{Args: []string{"/bin/ls", "-l", "/"}, Cwd: "/"})
 
-	res := mod3
+	res := mod4
 	dt, err := res.Marshal()
 	if err != nil {
 		panic(err)
