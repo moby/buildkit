8fd2d5431bc29ac9f0dd28fd84f584e23c2e4160
Tonis Tiigi <tonistiigi@gmail.com>
2018-03-19 11:52:52 -0700

    examples: fix gobuild example
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- examples/gobuild/main.go
+++ examples/gobuild/main.go
@@ -43,7 +43,7 @@ func run() error {
 	_ = buildkitd
 
 	containerd, err := gb.BuildExe(gobuild.BuildOpt{
-		Source:    llb.Git("github.com/containerd/containerd", "master"),
+		Source:    llb.Git("github.com/containerd/containerd", "v1.0.2"),
 		MountPath: "/go/src/github.com/containerd/containerd",
 		Pkg:       "github.com/containerd/containerd/cmd/containerd",
 		BuildTags: []string{"no_btrfs"},
