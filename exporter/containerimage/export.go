075c54589caa12a194f312e1edafdb02ff8d78fa
Ian Campbell <ijc@docker.com>
2018-08-06 10:23:46 +0100

    exporters: allow for more than one non-standard metadata key
    I think this was just a thinko.
    
    Signed-off-by: Ian Campbell <ijc@docker.com>

--- exporter/containerimage/export.go
+++ exporter/containerimage/export.go
@@ -78,8 +78,8 @@ func (e *imageExporter) Resolve(ctx context.Context, opt map[string]string) (exp
 		default:
 			if i.meta == nil {
 				i.meta = make(map[string][]byte)
-				i.meta[k] = []byte(v)
 			}
+			i.meta[k] = []byte(v)
 		}
 	}
 	return i, nil
