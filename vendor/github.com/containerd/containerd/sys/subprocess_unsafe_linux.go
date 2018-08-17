110c829d15cfff7a8bcfa2c5746c5dfe209c8223
Derek McGowan <derek@mcgstyle.net>
2018-08-17 13:12:02 -0700

    Update containerd v1.2 beta
    Signed-off-by: Derek McGowan <derek@mcgstyle.net>

--- vendor/github.com/containerd/containerd/sys/subprocess_unsafe_linux.go
+++ vendor/github.com/containerd/containerd/sys/subprocess_unsafe_linux.go
@@ -1,5 +1,3 @@
-// +build !windows
-
 /*
    Copyright The containerd Authors.
 
@@ -16,9 +14,17 @@
    limitations under the License.
 */
 
-package archive
+package sys
 
-// ApplyOptions provides additional options for an Apply operation
-type ApplyOptions struct {
-	Filter Filter // Filter tar headers
-}
+import (
+	_ "unsafe" // required for go:linkname.
+)
+
+//go:linkname beforeFork syscall.runtime_BeforeFork
+func beforeFork()
+
+//go:linkname afterFork syscall.runtime_AfterFork
+func afterFork()
+
+//go:linkname afterForkInChild syscall.runtime_AfterForkInChild
+func afterForkInChild()
