9dc04755dda5e4515ead8692ff466d2114e9f34d
Tonis Tiigi <tonistiigi@gmail.com>
2018-10-02 17:29:58 -0700

    executor: reset file limit in default spec
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- executor/oci/spec_unix.go
+++ executor/oci/spec_unix.go
@@ -44,6 +44,7 @@ func GenerateSpec(ctx context.Context, meta executor.Meta, mounts []executor.Mou
 	s.Process.Args = meta.Args
 	s.Process.Env = meta.Env
 	s.Process.Cwd = meta.Cwd
+	s.Process.Rlimits = nil // reset open files limit
 	s.Hostname = "buildkitsandbox"
 
 	s.Mounts = GetMounts(ctx,
