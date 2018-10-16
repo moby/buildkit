4c3050a26c2680fc413c21f0ef02a34b0b474697
Tonis Tiigi <tonistiigi@gmail.com>
2018-10-16 11:37:41 -0700

    dockerfile: setup master release deployment
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- frontend/dockerfile/cmd/dockerfile-frontend/main.go
+++ frontend/dockerfile/cmd/dockerfile-frontend/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	dockerfile "github.com/moby/buildkit/frontend/dockerfile/builder"
 	"github.com/moby/buildkit/frontend/gateway/grpcclient"
 	"github.com/moby/buildkit/util/appcontext"
@@ -8,6 +12,15 @@ import (
 )
 
 func main() {
+	var version bool
+	flag.BoolVar(&version, "version", false, "show version")
+	flag.Parse()
+
+	if version {
+		fmt.Printf("%s %s %s %s\n", os.Args[0], Package, Version, Revision)
+		os.Exit(0)
+	}
+
 	if err := grpcclient.RunFromEnvironment(appcontext.Context(), dockerfile.Build); err != nil {
 		logrus.Errorf("fatal error: %+v", err)
 		panic(err)
