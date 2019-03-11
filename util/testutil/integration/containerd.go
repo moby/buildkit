aece2ac1aa03c96886f0bc34ef23bef010f5e780
Tonis Tiigi <tonistiigi@gmail.com>
2019-03-10 21:04:50 -0700

    integration: disable gc
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- util/testutil/integration/containerd.go
+++ util/testutil/integration/containerd.go
@@ -123,6 +123,7 @@ disabled_plugins = ["cri"]
 
 	buildkitdArgs := []string{"buildkitd",
 		"--oci-worker=false",
+		"--containerd-worker-gc=false",
 		"--containerd-worker=true",
 		"--containerd-worker-addr", address,
 		"--containerd-worker-labels=org.mobyproject.buildkit.worker.sandbox=true", // Include use of --containerd-worker-labels to trigger https://github.com/moby/buildkit/pull/603
