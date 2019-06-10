e7ad765fcd6d31f0ea40dc9b8a0ca6be688c2657
Tonis Tiigi <tonistiigi@gmail.com>
2019-06-10 16:34:09 -0700

    executor: create directories as 711
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- executor/runcexecutor/executor.go
+++ executor/runcexecutor/executor.go
@@ -79,7 +79,7 @@ func New(opt Opt, networkProviders map[pb.NetMode]network.Provider) (executor.Ex
 
 	root := opt.Root
 
-	if err := os.MkdirAll(root, 0700); err != nil {
+	if err := os.MkdirAll(root, 0711); err != nil {
 		return nil, errors.Wrapf(err, "failed to create %s", root)
 	}
 
@@ -161,7 +161,7 @@ func (w *runcExecutor) Exec(ctx context.Context, meta executor.Meta, root cache.
 	id := identity.NewID()
 	bundle := filepath.Join(w.root, id)
 
-	if err := os.Mkdir(bundle, 0700); err != nil {
+	if err := os.Mkdir(bundle, 0711); err != nil {
 		return err
 	}
 	defer os.RemoveAll(bundle)
