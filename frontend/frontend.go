b92b5cf5f4405d171fd661cb9e6e7102621885ee
Cory Bennett <cbennett@netflix.com>
2020-07-17 01:04:21 +0000

    update FrontendLLBBridge to be composed of Executor
    Signed-off-by: Cory Bennett <cbennett@netflix.com>
    Signed-off-by: fahed dorgaa <fahed.dorgaa@gmail.com>

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -3,7 +3,6 @@ package frontend
 import (
 	"context"
 
-	"github.com/moby/buildkit/cache"
 	"github.com/moby/buildkit/client"
 	"github.com/moby/buildkit/client/llb"
 	"github.com/moby/buildkit/executor"
@@ -17,15 +16,9 @@ type Frontend interface {
 }
 
 type FrontendLLBBridge interface {
+	executor.Executor
 	Solve(ctx context.Context, req SolveRequest, sid string) (*Result, error)
 	ResolveImageConfig(ctx context.Context, ref string, opt llb.ResolveImageConfigOpt) (digest.Digest, []byte, error)
-	// Run will start a container for the given process with rootfs, mounts.
-	// `id` is an optional name for the container so it can be referenced later via Exec.
-	// `started` is an optional channel that will be closed when the container setup completes and has started running.
-	Run(ctx context.Context, id string, rootfs cache.Mountable, mounts []executor.Mount, process executor.ProcessInfo, started chan<- struct{}) error
-	// Exec will start a process in container matching `id`. An error will be returned
-	// if the container failed to start (via Run) or has exited before Exec is called.
-	Exec(ctx context.Context, id string, process executor.ProcessInfo) error
 }
 
 type SolveRequest = gw.SolveRequest
