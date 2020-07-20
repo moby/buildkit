2bd5d925ff4f960b95ec708ba29469c9b3f08304
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2020-07-20 20:28:13 +1000

    Provide separate NetProviders map for Windows
    This enforces the lack of host networking support on Windows.
    
    Signed-off-by: Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
    Signed-off-by: fahed dorgaa <fahed.dorgaa@gmail.com>

---
+++ util/network/netproviders/network_unix.go
@@ -0,0 +1,17 @@
+// +build !windows
+
+package netproviders
+
+import (
+	"github.com/moby/buildkit/util/network"
+	"github.com/sirupsen/logrus"
+)
+
+func getHostProvider() (network.Provider, bool) {
+	return network.NewHostProvider(), true
+}
+
+func getFallback() network.Provider {
+	logrus.Warn("using host network as the default")
+	return network.NewHostProvider()
+}
