dcb012845432b21a3e11fdfa2f6e99b714557e69
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2020-07-28 17:11:42 +1000

    Refactor OCI Spec generation to use oci.SpecOpts more
    This has the nice side-effect of unifying the mount- and
    non-mount-changes made due to processMode and securityMode.
    
    Signed-off-by: Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
    Signed-off-by: fahed dorgaa <fahed.dorgaa@gmail.com>

--- executor/oci/spec_unix.go
+++ executor/oci/spec_unix.go
@@ -5,26 +5,54 @@ package oci
 import (
 	"github.com/containerd/containerd/contrib/seccomp"
 	"github.com/containerd/containerd/oci"
+	"github.com/docker/docker/pkg/idtools"
 	"github.com/moby/buildkit/solver/pb"
 	"github.com/moby/buildkit/util/entitlements/security"
 	"github.com/moby/buildkit/util/system"
 	specs "github.com/opencontainers/runtime-spec/specs-go"
 )
 
+func generateMountOpts(resolvConf, hostsFile string) ([]oci.SpecOpts, error) {
+	return []oci.SpecOpts{
+		// https://github.com/moby/buildkit/issues/429
+		withRemovedMount("/run"),
+		withROBind(resolvConf, "/etc/resolv.conf"),
+		withROBind(hostsFile, "/etc/hosts"),
+		withCGroup(),
+	}, nil
+}
+
+// generateSecurityOpts may affect mounts, so must be called after generateMountOpts
 func generateSecurityOpts(mode pb.SecurityMode) ([]oci.SpecOpts, error) {
 	if mode == pb.SecurityMode_INSECURE {
-		return []oci.SpecOpts{security.WithInsecureSpec()}, nil
+		return []oci.SpecOpts{
+			security.WithInsecureSpec(),
+			oci.WithWriteableCgroupfs,
+			oci.WithWriteableSysfs,
+		}, nil
 	} else if system.SeccompSupported() && mode == pb.SecurityMode_SANDBOX {
 		return []oci.SpecOpts{seccomp.WithDefaultProfile()}, nil
 	}
 	return nil, nil
 }
 
+// generateProcessModeOpts may affect mounts, so must be called after generateMountOpts
 func generateProcessModeOpts(mode ProcessMode) ([]oci.SpecOpts, error) {
 	if mode == NoProcessSandbox {
-		// Mount for /proc is replaced in GetMounts() anyway
-		return []oci.SpecOpts{oci.WithHostNamespace(specs.PIDNamespace)}, nil
+		return []oci.SpecOpts{
+			oci.WithHostNamespace(specs.PIDNamespace),
+			withBoundProc(),
+		}, nil
 		// TODO(AkihiroSuda): Configure seccomp to disable ptrace (and prctl?) explicitly
 	}
 	return nil, nil
+}
+
+func generateIDmapOpts(idmap *idtools.IdentityMapping) ([]oci.SpecOpts, error) {
+	if idmap == nil {
+		return nil, nil
+	}
+	return []oci.SpecOpts{
+		oci.WithUserNamespace(specMapping(idmap.UIDs()), specMapping(idmap.GIDs())),
+	}, nil
 }
