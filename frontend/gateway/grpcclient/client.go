b392b06e5ceddb8307d9e9aec66c38344646c4ff
Cory Bennett <cbennett@netflix.com>
2020-10-02 21:35:51 +0000

    Simplify rpc exec exit error handling
    Signed-off-by: Cory Bennett <cbennett@netflix.com>
    Signed-off-by: fahed dorgaa <fahed.dorgaa@gmail.com>

--- frontend/gateway/grpcclient/client.go
+++ frontend/gateway/grpcclient/client.go
@@ -830,19 +830,13 @@ func (ctr *container) Start(ctx context.Context, req client.StartRequest) (clien
 				if exit.Code == 0 {
 					continue
 				}
-				if exit.Code == containerd.UnknownExitStatus {
-					exitError = grpcerrors.FromGRPC(status.ErrorProto(&spb.Status{
-						Code:    exit.Error.Code,
-						Message: exit.Error.Message,
-						Details: convertGogoAny(exit.Error.Details),
-					}))
-				} else {
-					exitError = errors.Wrap(
-						&errdefs.ExitError{
-							ExitCode: exit.Code,
-						},
-						exit.Error.GetMessage(),
-					)
+				exitError = grpcerrors.FromGRPC(status.ErrorProto(&spb.Status{
+					Code:    exit.Error.Code,
+					Message: exit.Error.Message,
+					Details: convertGogoAny(exit.Error.Details),
+				}))
+				if exit.Code != containerd.UnknownExitStatus {
+					exitError = &errdefs.ExitError{ExitCode: exit.Code, Err: exitError}
 				}
 			} else if serverDone := msg.GetDone(); serverDone != nil {
 				return exitError
