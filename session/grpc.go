bcc7233c3fd6e3dfa25a82e92b762a91f2e47f96
Ximo Guanter <ximo.guanter@gmail.com>
2020-10-03 22:58:21 +0200

    Make HTTP/2 authority pseudo-header spec-compliant
    Signed-off-by: Ximo Guanter <ximo.guanter@gmail.com>
    Signed-off-by: fahed dorgaa <fahed.dorgaa@gmail.com>

--- session/grpc.go
+++ session/grpc.go
@@ -64,7 +64,7 @@ func grpcClientConn(ctx context.Context, conn net.Conn) (context.Context, *grpc.
 		dialOpts = append(dialOpts, grpc.WithStreamInterceptor(grpc_middleware.ChainStreamClient(stream...)))
 	}
 
-	cc, err := grpc.DialContext(ctx, "", dialOpts...)
+	cc, err := grpc.DialContext(ctx, "localhost", dialOpts...)
 	if err != nil {
 		return nil, nil, errors.Wrap(err, "failed to create grpc client")
 	}
