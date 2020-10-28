d426cf66fbb5d7e3be20eab2f3b8115c08ef0eaf
ktock <ktokunaga.mail@gmail.com>
2020-10-28 23:10:14 +0900

    Bump stargz-snapshotter and partial registry logic integration
    Current stargz snapshotter pkg doesn't contain registry configuration and the
    client of that pkg needs to pass it to that pkg. So this commit includes changes
    of propagating buildkitd's registry configuration to stargz snapshotter.
    
    But this is a partial integration of registry logic between buildkitd and stargz
    snapshotter because buildkitd's session-based authentication logic is still not
    applied to stargz snapshotter. This means private images that require
    `~/.docker/config.json` creds can't be lazily pulled yet.
    
    Signed-off-by: ktock <ktokunaga.mail@gmail.com>
    Signed-off-by: fahed dorgaa <fahed.dorgaa@gmail.com>

--- cmd/buildkitd/main_oci_worker.go
+++ cmd/buildkitd/main_oci_worker.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"context"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -13,6 +14,7 @@ import (
 	snapshotsapi "github.com/containerd/containerd/api/services/snapshots/v1"
 	"github.com/containerd/containerd/defaults"
 	"github.com/containerd/containerd/pkg/dialer"
+	"github.com/containerd/containerd/remotes/docker"
 	ctdsnapshot "github.com/containerd/containerd/snapshots"
 	"github.com/containerd/containerd/snapshots/native"
 	"github.com/containerd/containerd/snapshots/overlay"
@@ -20,6 +22,7 @@ import (
 	"github.com/containerd/containerd/sys"
 	remotesn "github.com/containerd/stargz-snapshotter/snapshot"
 	"github.com/containerd/stargz-snapshotter/stargz"
+	sgzsource "github.com/containerd/stargz-snapshotter/stargz/source"
 	"github.com/moby/buildkit/cmd/buildkitd/config"
 	"github.com/moby/buildkit/executor/oci"
 	"github.com/moby/buildkit/util/network/cniprovider"
@@ -230,7 +233,8 @@ func ociWorkerInitializer(c *cli.Context, common workerInitializerOpt) ([]worker
 		return nil, err
 	}
 
-	snFactory, err := snapshotterFactory(common.config.Root, cfg)
+	hosts := resolverFunc(common.config)
+	snFactory, err := snapshotterFactory(common.config.Root, cfg, hosts)
 	if err != nil {
 		return nil, err
 	}
@@ -267,7 +271,7 @@ func ociWorkerInitializer(c *cli.Context, common workerInitializerOpt) ([]worker
 		return nil, err
 	}
 	opt.GCPolicy = getGCPolicy(cfg.GCConfig, common.config.Root)
-	opt.RegistryHosts = resolverFunc(common.config)
+	opt.RegistryHosts = hosts
 
 	if platformsStr := cfg.Platforms; len(platformsStr) != 0 {
 		platforms, err := parsePlatforms(platformsStr)
@@ -283,7 +287,7 @@ func ociWorkerInitializer(c *cli.Context, common workerInitializerOpt) ([]worker
 	return []worker.Worker{w}, nil
 }
 
-func snapshotterFactory(commonRoot string, cfg config.OCIConfig) (runc.SnapshotterFactory, error) {
+func snapshotterFactory(commonRoot string, cfg config.OCIConfig, hosts docker.RegistryHosts) (runc.SnapshotterFactory, error) {
 	var (
 		name    = cfg.Snapshotter
 		address = cfg.ProxySnapshotterPath
@@ -348,13 +352,36 @@ func snapshotterFactory(commonRoot string, cfg config.OCIConfig) (runc.Snapshott
 			return fuseoverlayfs.NewSnapshotter(root)
 		}
 	case "stargz":
-		snFactory.New = func(root string) (ctdsnapshot.Snapshotter, error) {
-			fs, err := stargz.NewFilesystem(filepath.Join(root, "stargz"),
-				cfg.StargzSnapshotterConfig)
+		// Pass the registry configuration to stargz snapshotter
+		sgzhosts := func(host string) ([]docker.RegistryHost, error) {
+			base, err := hosts(host)
 			if err != nil {
 				return nil, err
 			}
-			return remotesn.NewSnapshotter(filepath.Join(root, "snapshotter"),
+			for i := range base {
+				if base[i].Authorizer == nil {
+					// Default authorizer that don't fetch creds via session
+					// TODO(ktock): use session-based authorizer
+					base[i].Authorizer = docker.NewDockerAuthorizer(
+						docker.WithAuthClient(base[i].Client))
+				}
+			}
+			return base, nil
+		}
+		snFactory.New = func(root string) (ctdsnapshot.Snapshotter, error) {
+			fs, err := stargz.NewFilesystem(filepath.Join(root, "stargz"),
+				cfg.StargzSnapshotterConfig,
+				stargz.WithGetSources(
+					// provides source info based on the registry config and
+					// default labels.
+					sgzsource.FromDefaultLabels(sgzhosts),
+				),
+			)
+			if err != nil {
+				return nil, err
+			}
+			return remotesn.NewSnapshotter(context.Background(),
+				filepath.Join(root, "snapshotter"),
 				fs, remotesn.AsynchronousRemove)
 		}
 	default:
