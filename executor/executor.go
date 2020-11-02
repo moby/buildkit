5952f6a8e829afcceaf141e02afd3c69d08ce128
Tonis Tiigi <tonistiigi@gmail.com>
2020-11-01 23:55:46 -0800

    executor: change mount to struct
    Allows readonly passed cleanly.
    
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>
    Signed-off-by: fahed dorgaa <fahed.dorgaa@gmail.com>

--- executor/executor.go
+++ executor/executor.go
@@ -49,7 +49,7 @@ type Executor interface {
 	// Run will start a container for the given process with rootfs, mounts.
 	// `id` is an optional name for the container so it can be referenced later via Exec.
 	// `started` is an optional channel that will be closed when the container setup completes and has started running.
-	Run(ctx context.Context, id string, rootfs Mountable, mounts []Mount, process ProcessInfo, started chan<- struct{}) error
+	Run(ctx context.Context, id string, rootfs Mount, mounts []Mount, process ProcessInfo, started chan<- struct{}) error
 	// Exec will start a process in container matching `id`. An error will be returned
 	// if the container failed to start (via Run) or has exited before Exec is called.
 	Exec(ctx context.Context, id string, process ProcessInfo) error
