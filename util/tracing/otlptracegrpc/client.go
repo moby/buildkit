90a2b50cfda805e7d31abf8fe9a8a6105013a780
Sebastiaan van Stijn <github@gone.nl>
2021-07-28 13:09:49 +0200

    util/tracing: remove incorrect import enforcing comment
    This import comment caused compilation of buildx to fail if `GO111MODULE` was
    set to `off`:
    
    Without `GO111MODULE` set (but with `-mod=vendor`:
    
        echo $GO111MODULE
    
        export PKG=github.com/docker/buildx
        export LDFLAGS="-X ${PKG}/version.Version=$(git describe --match 'v[0-9]*' --always --tags) -X ${PKG}/version.Revision=$(git rev-parse HEAD) -X ${PKG}/version.Package=${PKG}"
        GOFLAGS=-mod=vendor go build -o bin/docker-buildx -ldflags "${LDFLAGS}" ./cmd/buildx
        bin/docker-buildx version
        github.com/docker/buildx v0.6.0 d9ee3b134cbc2d09513fa7fee4176a3919e05887
    
    When setting `GO111MODULE=off`, it fails on the incorrect import path in the
    vendored file (looks like GO111MODULE=on ignores import-path comments?):
    
        export GO111MODULE=off
        root@5a55ec1c1eed:/go/src/github.com/docker/buildx# GOFLAGS=-mod=vendor go build -o bin/docker-buildx -ldflags "${LDFLAGS}" ./cmd/buildx
        vendor/github.com/moby/buildkit/client/client.go:20:2: code in directory /go/src/github.com/docker/buildx/vendor/github.com/moby/buildkit/util/tracing/otlptracegrpc expects import "go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
        vendor/go.opentelemetry.io/otel/exporters/otlp/otlptrace/internal/connection/connection.go:33:2: found import comments "go.opentelemetry.io/otel/exporters/otlp/internal/otlpconfig" (options.go) and "go.opentelemetry.io/otel/exporters/otlp/otlptrace/internal/otlpconfig" (optiontypes.go) in /go/src/github.com/docker/buildx/vendor/go.opentelemetry.io/otel/exporters/otlp/otlptrace/internal/otlpconfig
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- util/tracing/otlptracegrpc/client.go
+++ util/tracing/otlptracegrpc/client.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package otlptracegrpc // import "go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+package otlptracegrpc
 
 import (
 	"context"
