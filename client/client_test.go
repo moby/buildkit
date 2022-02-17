d67243ed0b51ab05c6f212ada902f93f5dfb92eb
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2022-02-16 17:15:29 -0800

    Fix invalid layer index error
    Signed-off-by: Vlad A. Ionescu <vladaionescu@users.noreply.github.com>

--- client/client_test.go
+++ client/client_test.go
@@ -2798,7 +2798,7 @@ func testBuildPushAndValidate(t *testing.T, sb integration.Sandbox) {
 	require.NotEqual(t, "", ociimg.Architecture)
 	require.NotEqual(t, "", ociimg.Config.WorkingDir)
 	require.Equal(t, "layers", ociimg.RootFS.Type)
-	require.Equal(t, 2, len(ociimg.RootFS.DiffIDs))
+	require.Equal(t, 3, len(ociimg.RootFS.DiffIDs))
 	require.NotNil(t, ociimg.Created)
 	require.True(t, time.Since(*ociimg.Created) < 2*time.Minute)
 	require.Condition(t, func() bool {
@@ -2815,7 +2815,7 @@ func testBuildPushAndValidate(t *testing.T, sb integration.Sandbox) {
 	require.Contains(t, ociimg.History[1].CreatedBy, "true")
 	require.Contains(t, ociimg.History[2].CreatedBy, "foo/sub/baz")
 	require.False(t, ociimg.History[0].EmptyLayer)
-	require.True(t, ociimg.History[1].EmptyLayer)
+	require.False(t, ociimg.History[1].EmptyLayer)
 	require.False(t, ociimg.History[2].EmptyLayer)
 
 	dt, err = content.ReadBlob(ctx, img.ContentStore(), img.Target())
@@ -2830,7 +2830,7 @@ func testBuildPushAndValidate(t *testing.T, sb integration.Sandbox) {
 	require.NoError(t, err)
 
 	require.Equal(t, images.MediaTypeDockerSchema2Manifest, mfst.MediaType)
-	require.Equal(t, 2, len(mfst.Layers))
+	require.Equal(t, 3, len(mfst.Layers))
 	require.Equal(t, images.MediaTypeDockerSchema2LayerGzip, mfst.Layers[0].MediaType)
 	require.Equal(t, images.MediaTypeDockerSchema2LayerGzip, mfst.Layers[1].MediaType)
 
@@ -2857,7 +2857,7 @@ func testBuildPushAndValidate(t *testing.T, sb integration.Sandbox) {
 	_, ok = m["foo/sub/baz"]
 	require.False(t, ok)
 
-	dt, err = content.ReadBlob(ctx, img.ContentStore(), ocispecs.Descriptor{Digest: mfst.Layers[1].Digest})
+	dt, err = content.ReadBlob(ctx, img.ContentStore(), ocispecs.Descriptor{Digest: mfst.Layers[2].Digest})
 	require.NoError(t, err)
 
 	m, err = testutil.ReadTarToMap(dt, true)
