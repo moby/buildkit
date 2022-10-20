b34da50b4c5040adb08a76a7a962a55285664ee4
CrazyMax <crazy-max@users.noreply.github.com>
2022-10-20 20:45:49 +0200

    testNamedImageContextPlatform skip direct push with dockerd
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>
    (cherry picked from commit 5505af901804c2d493551609e05327e0f7d0be10)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- frontend/dockerfile/dockerfile_test.go
+++ frontend/dockerfile/dockerfile_test.go
@@ -5437,6 +5437,7 @@ COPY --from=base /env_foobar /
 }
 
 func testNamedImageContextPlatform(t *testing.T, sb integration.Sandbox) {
+	integration.SkipIfDockerd(t, sb, "direct push")
 	ctx := sb.Context()
 
 	c, err := client.New(ctx, sb.Address())
