05c4d571cf97fd0878fecc074972a410af889ca9
CrazyMax <crazy-max@users.noreply.github.com>
2023-01-07 15:44:25 +0100

    hack: remove s3_test
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

--- hack/s3_test/run_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -ex
-
-cd "$(dirname "$0")"
-
-docker buildx bake --load
-docker run --rm --privileged -p 9001:9001 -p 8060:8060 moby/buildkit:s3test /test/test.sh
-docker rmi moby/buildkit:s3test
