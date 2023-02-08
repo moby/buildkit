ab6e4c61d519e1ac52c7c1798a219685091bc92b
CrazyMax <crazy-max@users.noreply.github.com>
2023-02-08 11:35:59 +0100

    gitutil: use subtests
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>
    (cherry picked from commit b5c91835950130e3b88a67644b3aef8b2acc16fc)

--- util/gitutil/git_ref_test.go
+++ util/gitutil/git_ref_test.go
@@ -6,72 +6,127 @@ import (
 )
 
 func TestParseGitRef(t *testing.T) {
-	cases := map[string]*GitRef{
-		"https://example.com/":    nil,
-		"https://example.com/foo": nil,
-		"https://example.com/foo.git": {
-			Remote:    "https://example.com/foo.git",
-			ShortName: "foo",
+	cases := []struct {
+		ref      string
+		expected *GitRef
+	}{
+		{
+			ref:      "https://example.com/",
+			expected: nil,
 		},
-		"https://example.com/foo.git#deadbeef": {
-			Remote:    "https://example.com/foo.git",
-			ShortName: "foo",
-			Commit:    "deadbeef",
+		{
+			ref:      "https://example.com/foo",
+			expected: nil,
 		},
-		"https://example.com/foo.git#release/1.2": {
-			Remote:    "https://example.com/foo.git",
-			ShortName: "foo",
-			Commit:    "release/1.2",
+		{
+			ref: "https://example.com/foo.git",
+			expected: &GitRef{
+				Remote:    "https://example.com/foo.git",
+				ShortName: "foo",
+			},
 		},
-		"https://example.com/foo.git/":    nil,
-		"https://example.com/foo.git.bar": nil,
-		"git://example.com/foo": {
-			Remote:         "git://example.com/foo",
-			ShortName:      "foo",
-			UnencryptedTCP: true,
+		{
+			ref: "https://example.com/foo.git#deadbeef",
+			expected: &GitRef{
+				Remote:    "https://example.com/foo.git",
+				ShortName: "foo",
+				Commit:    "deadbeef",
+			},
 		},
-		"github.com/moby/buildkit": {
-			Remote: "github.com/moby/buildkit", ShortName: "buildkit",
-			IndistinguishableFromLocal: true,
+		{
+			ref: "https://example.com/foo.git#release/1.2",
+			expected: &GitRef{
+				Remote:    "https://example.com/foo.git",
+				ShortName: "foo",
+				Commit:    "release/1.2",
+			},
 		},
-		"https://github.com/moby/buildkit": nil,
-		"https://github.com/moby/buildkit.git": {
-			Remote:    "https://github.com/moby/buildkit.git",
-			ShortName: "buildkit",
+		{
+			ref:      "https://example.com/foo.git/",
+			expected: nil,
 		},
-		"git@github.com:moby/buildkit": {
-			Remote:    "git@github.com:moby/buildkit",
-			ShortName: "buildkit",
+		{
+			ref:      "https://example.com/foo.git.bar",
+			expected: nil,
 		},
-		"git@github.com:moby/buildkit.git": {
-			Remote:    "git@github.com:moby/buildkit.git",
-			ShortName: "buildkit",
+		{
+			ref: "git://example.com/foo",
+			expected: &GitRef{
+				Remote:         "git://example.com/foo",
+				ShortName:      "foo",
+				UnencryptedTCP: true,
+			},
 		},
-		"git@bitbucket.org:atlassianlabs/atlassian-docker.git": {
-			Remote:    "git@bitbucket.org:atlassianlabs/atlassian-docker.git",
-			ShortName: "atlassian-docker",
+		{
+			ref: "github.com/moby/buildkit",
+			expected: &GitRef{
+				Remote:                     "github.com/moby/buildkit",
+				ShortName:                  "buildkit",
+				IndistinguishableFromLocal: true,
+			},
 		},
-		"https://github.com/foo/bar.git#baz/qux:quux/quuz": {
-			Remote:    "https://github.com/foo/bar.git",
-			ShortName: "bar",
-			Commit:    "baz/qux",
-			SubDir:    "quux/quuz",
+		{
+			ref:      "https://github.com/moby/buildkit",
+			expected: nil,
 		},
-		"http://github.com/docker/docker.git:#branch": nil,
+		{
+			ref: "https://github.com/moby/buildkit.git",
+			expected: &GitRef{
+				Remote:    "https://github.com/moby/buildkit.git",
+				ShortName: "buildkit",
+			},
+		},
+		{
+			ref: "git@github.com:moby/buildkit",
+			expected: &GitRef{
+				Remote:    "git@github.com:moby/buildkit",
+				ShortName: "buildkit",
+			},
+		},
+		{
+			ref: "git@github.com:moby/buildkit.git",
+			expected: &GitRef{
+				Remote:    "git@github.com:moby/buildkit.git",
+				ShortName: "buildkit",
+			},
+		},
+		{
+			ref: "git@bitbucket.org:atlassianlabs/atlassian-docker.git",
+			expected: &GitRef{
+				Remote:    "git@bitbucket.org:atlassianlabs/atlassian-docker.git",
+				ShortName: "atlassian-docker",
+			},
+		},
+		{
+			ref: "https://github.com/foo/bar.git#baz/qux:quux/quuz",
+			expected: &GitRef{
+				Remote:    "https://github.com/foo/bar.git",
+				ShortName: "bar",
+				Commit:    "baz/qux",
+				SubDir:    "quux/quuz",
+			},
+		},
+		{
+			ref:      "http://github.com/docker/docker.git:#branch",
+			expected: nil,
+		},
 	}
-	for ref, expected := range cases {
-		got, err := ParseGitRef(ref)
-		if expected == nil {
-			if err == nil {
-				t.Errorf("expected an error for ParseGitRef(%q)", ref)
+	for _, tt := range cases {
+		tt := tt
+		t.Run(tt.ref, func(t *testing.T) {
+			got, err := ParseGitRef(tt.ref)
+			if tt.expected == nil {
+				if err == nil {
+					t.Errorf("expected an error for ParseGitRef(%q)", tt.ref)
+				}
+			} else {
+				if err != nil {
+					t.Errorf("got an unexpected error: ParseGitRef(%q): %v", tt.ref, err)
+				}
+				if !reflect.DeepEqual(got, tt.expected) {
+					t.Errorf("expected ParseGitRef(%q) to return %#v, got %#v", tt.ref, tt.expected, got)
+				}
 			}
-		} else {
-			if err != nil {
-				t.Errorf("got an unexpected error: ParseGitRef(%q): %v", ref, err)
-			}
-			if !reflect.DeepEqual(got, expected) {
-				t.Errorf("expected ParseGitRef(%q) to return %#v, got %#v", ref, expected, got)
-			}
-		}
+		})
 	}
 }
