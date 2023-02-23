dadf889cba4aa8e19cf65556be55c480e70f8cf9
Gabriel <samfiragabriel@gmail.com>
2023-02-23 13:51:15 +0200

    Update cmd/buildkitd/main_windows.go
    Co-authored-by: Akihiro Suda <suda.kyoto@gmail.com>
    Signed-off-by: Gabriel <samfiragabriel@gmail.com>
    Signed-off-by: Edson Michaque <edson@michaque.com>

--- cmd/buildkitd/main_windows.go
+++ cmd/buildkitd/main_windows.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	defaultTraceSocketPath = `\\.\pipe\otel-grpc`
+	defaultTraceSocketPath = `\\.\pipe\buildkit-otel-grpc`
 )
 
 func listenFD(addr string, tlsConfig *tls.Config) (net.Listener, error) {
