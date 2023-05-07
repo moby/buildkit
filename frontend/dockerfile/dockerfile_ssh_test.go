30ea41976de19966b3a10ce028517c29c62c8e39
Erik Sipsma <erik@sipsma.dev>
2023-05-07 13:13:36 -0700

    ssh: add fallback to ensure conn is closed in all cases.
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- frontend/dockerfile/dockerfile_ssh_test.go
+++ frontend/dockerfile/dockerfile_ssh_test.go
@@ -1,13 +1,16 @@
 package dockerfile
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/containerd/continuity/fs/fstest"
 	"github.com/moby/buildkit/client"
@@ -20,6 +23,7 @@ import (
 
 var sshTests = integration.TestFuncs(
 	testSSHSocketParams,
+	testSSHFileDescriptorsClosed,
 )
 
 func init() {
@@ -72,4 +76,75 @@ RUN --mount=type=ssh,mode=741,uid=100,gid=102 [ "$(stat -c "%u %g %f" $SSH_AUTH_
 		Session: []session.Attachable{ssh},
 	}, nil)
 	require.NoError(t, err)
+}
+
+func testSSHFileDescriptorsClosed(t *testing.T, sb integration.Sandbox) {
+	f := getFrontend(t, sb)
+
+	dockerfile := []byte(`
+FROM alpine
+RUN --mount=type=ssh apk update \
+ && apk add openssh-client-default \
+ && mkdir -p -m 0600 ~/.ssh \
+ && ssh-keyscan github.com >> ~/.ssh/known_hosts \
+ && for i in $(seq 1 3); do \
+        ssh -T git@github.com; \
+    done; \
+    exit 0;
+`)
+
+	dir, err := integration.Tmpdir(
+		t,
+		fstest.CreateFile("Dockerfile", dockerfile, 0600),
+	)
+	require.NoError(t, err)
+
+	c, err := client.New(sb.Context(), sb.Address())
+	require.NoError(t, err)
+	defer c.Close()
+
+	// not using t.TempDir() here because the path ends up longer than the unix socket max length
+	tmpDir, err := os.MkdirTemp("", "buildkit-ssh-test-")
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		os.RemoveAll(tmpDir)
+	})
+	sockPath := filepath.Join(tmpDir, "ssh-agent.sock")
+
+	sshAgentCmd := exec.CommandContext(sb.Context(), "ssh-agent", "-s", "-d", "-a", sockPath)
+	sshAgentOutputBuf := &bytes.Buffer{}
+	sshAgentCmd.Stderr = sshAgentOutputBuf
+	require.NoError(t, sshAgentCmd.Start())
+	var found bool
+	for i := 0; i < 100; i++ {
+		_, err := os.Stat(sockPath)
+		if err == nil {
+			found = true
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	if !found {
+		sshAgentOutput := sshAgentOutputBuf.String()
+		t.Fatalf("ssh-agent failed to start: %s", sshAgentOutput)
+	}
+
+	ssh, err := sshprovider.NewSSHAgentProvider([]sshprovider.AgentConfig{{
+		Paths: []string{sockPath},
+	}})
+	require.NoError(t, err)
+
+	_, err = f.Solve(sb.Context(), c, client.SolveOpt{
+		LocalDirs: map[string]string{
+			dockerui.DefaultLocalNameDockerfile: dir,
+			dockerui.DefaultLocalNameContext:    dir,
+		},
+		Session: []session.Attachable{ssh},
+	}, nil)
+	require.NoError(t, err)
+
+	sshAgentOutput := sshAgentOutputBuf.String()
+	require.Contains(t, sshAgentOutput, "process_message: socket 1")
+	require.NotContains(t, sshAgentOutput, "process_message: socket 2")
+	require.NotContains(t, sshAgentOutput, "process_message: socket 3")
 }
