5a82dd3e8b8afe292c4bcb11cadc0f16b32aa412
Justin Chadwell <me@jedevc.com>
2023-10-11 16:21:49 +0100

    gateway: restore original filename in StatFile error message
    Follow-up to 38040ae03e5294cc2510aa84954b49bc5a8c7374.
    
    Messages returned by fsutil.Stat are guaranteed to return a PathError
    wrapped by a stack trace (since it calls os.Lstat). However, as these
    error messages are printed, they include the temporary directory for the
    mounted reference which is not useful to the caller.
    
    On an error, we can restore the filename in the PathError to the
    requested filename, as also seen in os.DirFS.
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- cache/util/fsutil.go
+++ cache/util/fsutil.go
@@ -128,6 +128,16 @@ func StatFile(ctx context.Context, mount snapshot.Mountable, path string) (*fsty
 			return errors.WithStack(err)
 		}
 		if st, err = fsutil.Stat(fp); err != nil {
+			// The filename here is internal to the mount, so we can restore
+			// the request base path for error reporting.
+			// See os.DirFS.Open for details.
+			err1 := err
+			if err := errors.Cause(err); err != nil {
+				err1 = err
+			}
+			if pe, ok := err1.(*os.PathError); ok {
+				pe.Path = path
+			}
 			return errors.WithStack(err)
 		}
 		return nil
