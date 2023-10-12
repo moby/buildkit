938560b7bd7e41be6f1a12ca26e34be0606084d6
Justin Chadwell <me@jedevc.com>
2023-10-12 12:06:27 +0100

    sshutil: refactor to allow parsing scp-style urls
    SCP-style URLs take the following format [1]:
    
    	[user@]server:project.git
    
    These are used and handled by git, so we should have a full-parser for
    these, that return a custom URL type.
    
    [1]: https://git-scm.com/book/en/v2/Git-on-the-Server-The-Protocols
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- util/sshutil/scpurl_test.go
+++ util/sshutil/scpurl_test.go
@@ -24,3 +24,50 @@ func TestIsImplicitSSHTransport(t *testing.T) {
 	// explicit definitions are checked via isGitTransport, and are not implicit therefore this should fail:
 	require.False(t, IsImplicitSSHTransport("ssh://root@subdomain.example.hostname:2222/root/my/really/weird/path/foo.git"))
 }
+
+func TestParseSCPStyleURL(t *testing.T) {
+	tests := []struct {
+		url      string
+		user     string
+		host     string
+		path     string
+		fragment string
+		err      bool
+	}{
+		{
+			url: "http://github.com/moby/buildkit",
+			err: true,
+		},
+		{
+			url: "ssh://git@github.com:22/moby/buildkit.git",
+			err: true,
+		},
+		{
+			url:  "git@github.com:moby/buildkit.git",
+			host: "github.com",
+			path: "moby/buildkit.git",
+			user: "git",
+		},
+		{
+			url:      "git@github.com:moby/buildkit.git#v1.0",
+			host:     "github.com",
+			path:     "moby/buildkit.git",
+			user:     "git",
+			fragment: "v1.0",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.url, func(t *testing.T) {
+			remote, err := ParseSCPStyleURL(test.url)
+			if test.err {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, test.user, remote.User.String())
+				require.Equal(t, test.host, remote.Host)
+				require.Equal(t, test.path, remote.Path)
+				require.Equal(t, test.fragment, remote.Fragment)
+			}
+		})
+	}
+}
