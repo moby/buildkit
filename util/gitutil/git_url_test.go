moby/buildkit

commit 50e75e3565b0e301a364d64000c4739e1abdc5f1

git: update parsing to clarify between scp-style urls
This should also resolve the ports parsing issue.

Co-authored-by: Aaron Lehmann <alehmann@netflix.com>
Signed-off-by: Justin Chadwell <me@jedevc.com>

client/llb/source.go
@@ -5,6 +5,7 @@ import (
 	_ "crypto/sha256" // for opencontainers/go-digest
 	"encoding/json"
 	"os"
+	"path"
 	"strconv"
 	"strings"
 
@@ -237,29 +238,30 @@ type ImageInfo struct {
 //
 // By default the git repository is cloned with `--depth=1` to reduce the amount of data downloaded.
 // Additionally the ".git" directory is removed after the clone, you can keep ith with the [KeepGitDir] [GitOption].
-func Git(remote, ref string, opts ...GitOption) State {
-	url := strings.Split(remote, "#")[0]
-
-	var protocolType int
-	remote, protocolType = gitutil.ParseProtocol(remote)
-
-	var sshHost string
-	if protocolType == gitutil.SSHProtocol {
-		parts := strings.SplitN(remote, ":", 2)
-		if len(parts) == 2 {
-			sshHost = parts[0]
-			// keep remote consistent with http(s) version
-			remote = parts[0] + "/" + parts[1]
-		}
-	}
-	if protocolType == gitutil.UnknownProtocol {
+func Git(url, ref string, opts ...GitOption) State {
+	remote, err := gitutil.ParseURL(url)
+	if errors.Is(err, gitutil.ErrUnknownProtocol) {
 		url = "https://" + url
+		remote, err = gitutil.ParseURL(url)
+	}
+	if remote != nil {
+		remote.Fragment = ""
+		url = remote.String()
 	}
 
-	id := remote
-
-	if ref != "" {
-		id += "#" + ref
+	var id string
+	if err != nil {
+		// If we can't parse the URL, just use the full URL as the ID. The git
+		// operation will fail later on.
+		id = url
+	} else {
+		// We construct the ID manually here, so that we can create the same ID
+		// for different protocols (e.g. https and ssh) that have the same
+		// host/path/fragment combination.
+		id = remote.Host + path.Join("/", remote.Path)
+		if ref != "" {
+			id += "#" + ref
+		}
 	}
 
 	gi := &GitInfo{
@@ -290,11 +292,11 @@ func Git(remote, ref string, opts ...GitOption) State {
 			addCap(&gi.Constraints, pb.CapSourceGitHTTPAuth)
 		}
 	}
-	if protocolType == gitutil.SSHProtocol {
+	if remote != nil && remote.Scheme == gitutil.SSHProtocol {
 		if gi.KnownSSHHosts != "" {
 			attrs[pb.AttrKnownSSHHosts] = gi.KnownSSHHosts
-		} else if sshHost != "" {
-			keyscan, err := sshutil.SSHKeyScan(sshHost)
+		} else {
+			keyscan, err := sshutil.SSHKeyScan(remote.Host)
 			if err == nil {
 				// best effort
 				attrs[pb.AttrKnownSSHHosts] = keyscan

source/git/identifier.go
@@ -1,13 +1,13 @@
 package git
 
 import (
-	"net/url"
 	"path"
 	"strings"
 
 	"github.com/moby/buildkit/solver/llbsolver/provenance"
 	"github.com/moby/buildkit/source"
 	srctypes "github.com/moby/buildkit/source/types"
+	"github.com/moby/buildkit/util/gitutil"
 	"github.com/moby/buildkit/util/sshutil"
 )
 
@@ -23,32 +23,19 @@ type GitIdentifier struct {
 }
 
 func NewGitIdentifier(remoteURL string) (*GitIdentifier, error) {
-	repo := GitIdentifier{}
-
 	if !isGitTransport(remoteURL) {
 		remoteURL = "https://" + remoteURL
 	}
-
-	var fragment string
-	if sshutil.IsImplicitSSHTransport(remoteURL) {
-		// implicit ssh urls such as "git@.." are not actually a URL, so cannot be parsed as URL
-		parts := strings.SplitN(remoteURL, "#", 2)
-
-		repo.Remote = parts[0]
-		if len(parts) == 2 {
-			fragment = parts[1]
-		}
-		repo.Ref, repo.Subdir = getRefAndSubdir(fragment)
-	} else {
-		u, err := url.Parse(remoteURL)
-		if err != nil {
-			return nil, err
-		}
-
-		repo.Ref, repo.Subdir = getRefAndSubdir(u.Fragment)
-		u.Fragment = ""
-		repo.Remote = u.String()
+	u, err := gitutil.ParseURL(remoteURL)
+	if err != nil {
+		return nil, err
 	}
+
+	repo := GitIdentifier{}
+	repo.Ref, repo.Subdir = gitutil.SplitGitFragment(u.Fragment)
+	u.Fragment = ""
+	repo.Remote = u.String()
+
 	if sd := path.Clean(repo.Subdir); sd == "/" || sd == "." {
 		repo.Subdir = ""
 	}
@@ -95,16 +82,4 @@ func (id *GitIdentifier) Capture(c *provenance.Capture, pin string) error {
 // the prefix of the string for known protocols used in git.
 func isGitTransport(str string) bool {
 	return strings.HasPrefix(str, "http://") || strings.HasPrefix(str, "https://") || strings.HasPrefix(str, "git://") || strings.HasPrefix(str, "ssh://") || sshutil.IsImplicitSSHTransport(str)
-}
-
-func getRefAndSubdir(fragment string) (ref string, subdir string) {
-	refAndDir := strings.SplitN(fragment, ":", 2)
-	ref = ""
-	if len(refAndDir[0]) != 0 {
-		ref = refAndDir[0]
-	}
-	if len(refAndDir) > 1 && len(refAndDir[1]) != 0 {
-		subdir = refAndDir[1]
-	}
-	return
 }

util/gitutil/git_ref.go
@@ -1,10 +1,12 @@
 package gitutil
 
 import (
+	"net/url"
 	"regexp"
 	"strings"
 
 	"github.com/containerd/containerd/errdefs"
+	"github.com/pkg/errors"
 )
 
 // GitRef represents a git ref.
@@ -51,19 +53,33 @@ type GitRef struct {
 func ParseGitRef(ref string) (*GitRef, error) {
 	res := &GitRef{}
 
+	var (
+		remote *url.URL
+		err    error
+	)
+
 	if strings.HasPrefix(ref, "github.com/") {
 		res.IndistinguishableFromLocal = true // Deprecated
-	} else {
-		_, proto := ParseProtocol(ref)
-		switch proto {
-		case UnknownProtocol:
-			return nil, errdefs.ErrInvalidArgument
+		remote = &url.URL{
+			Scheme: "https",
+			Host:   "github.com",
+			Path:   strings.TrimPrefix(ref, "github.com/"),
 		}
-		switch proto {
+	} else {
+		remote, err = ParseURL(ref)
+		if errors.Is(err, ErrUnknownProtocol) {
+			remote, err = ParseURL("https://" + ref)
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		switch remote.Scheme {
 		case HTTPProtocol, GitProtocol:
 			res.UnencryptedTCP = true // Discouraged, but not deprecated
 		}
-		switch proto {
+
+		switch remote.Scheme {
 		// An HTTP(S) URL is considered to be a valid git ref only when it has the ".git[...]" suffix.
 		case HTTPProtocol, HTTPSProtocol:
 			var gitURLPathWithFragmentSuffix = regexp.MustCompile(`\.git(?:#.+)?$`)
@@ -73,13 +89,16 @@ func ParseGitRef(ref string) (*GitRef, error) {
 		}
 	}
 
-	var fragment string
-	res.Remote, fragment, _ = strings.Cut(ref, "#")
-	if len(res.Remote) == 0 {
-		return res, errdefs.ErrInvalidArgument
+	res.Commit, res.SubDir = SplitGitFragment(remote.Fragment)
+	remote.Fragment = ""
+
+	res.Remote = remote.String()
+	if res.IndistinguishableFromLocal {
+		_, res.Remote, _ = strings.Cut(res.Remote, "://")
 	}
-	res.Commit, res.SubDir, _ = strings.Cut(fragment, ":")
+
 	repoSplitBySlash := strings.Split(res.Remote, "/")
 	res.ShortName = strings.TrimSuffix(repoSplitBySlash[len(repoSplitBySlash)-1], ".git")
+
 	return res, nil
 }

util/gitutil/git_url.go
@@ -1,0 +1,71 @@
+package gitutil
+
+import (
+	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/moby/buildkit/util/sshutil"
+	"github.com/pkg/errors"
+)
+
+const (
+	HTTPProtocol  string = "http"
+	HTTPSProtocol string = "https"
+	SSHProtocol   string = "ssh"
+	GitProtocol   string = "git"
+)
+
+var (
+	ErrUnknownProtocol = errors.New("unknown protocol")
+	ErrInvalidProtocol = errors.New("invalid protocol")
+)
+
+var supportedProtos = map[string]struct{}{
+	HTTPProtocol:  {},
+	HTTPSProtocol: {},
+	SSHProtocol:   {},
+	GitProtocol:   {},
+}
+
+var protoRegexp = regexp.MustCompile(`^[a-zA-Z0-9]+://`)
+
+// ParseURL parses a git URL and returns a parsed URL object.
+//
+// ParseURL understands implicit ssh URLs such as "git@host:repo", and
+// returns the same response as if the URL were "ssh://git@host/repo".
+func ParseURL(remote string) (*url.URL, error) {
+	if proto := protoRegexp.FindString(remote); proto != "" {
+		proto = strings.ToLower(strings.TrimSuffix(proto, "://"))
+		if _, ok := supportedProtos[proto]; !ok {
+			return nil, errors.Wrap(ErrInvalidProtocol, proto)
+		}
+
+		return url.Parse(remote)
+	}
+
+	if sshutil.IsImplicitSSHTransport(remote) {
+		remote, fragment, _ := strings.Cut(remote, "#")
+		remote, path, _ := strings.Cut(remote, ":")
+		user, host, _ := strings.Cut(remote, "@")
+		if !strings.HasPrefix(path, "/") {
+			path = "/" + path
+		}
+		return &url.URL{
+			Scheme:   SSHProtocol,
+			User:     url.User(user),
+			Host:     host,
+			Path:     path,
+			Fragment: fragment,
+		}, nil
+	}
+
+	return nil, ErrUnknownProtocol
+}
+
+// SplitGitFragments splits a git URL fragment into its respective git
+// reference and subdirectory components.
+func SplitGitFragment(fragment string) (ref string, subdir string) {
+	ref, subdir, _ = strings.Cut(fragment, ":")
+	return ref, subdir
+}

source/git/identifier_test.go
@@ -27,7 +27,7 @@ func TestNewGitIdentifier(t *testing.T) {
 		{
 			url: "git@github.com:moby/buildkit.git",
 			expected: GitIdentifier{
-				Remote: "git@github.com:moby/buildkit.git",
+				Remote: "ssh://git@github.com/moby/buildkit.git",
 			},
 		},
 		{
@@ -75,13 +75,13 @@ func TestNewGitIdentifier(t *testing.T) {
 		{
 			url: "git@github.com:user/repo.git",
 			expected: GitIdentifier{
-				Remote: "git@github.com:user/repo.git",
+				Remote: "ssh://git@github.com/user/repo.git",
 			},
 		},
 		{
 			url: "git@github.com:user/repo.git#mybranch:mydir/mysubdir/",
 			expected: GitIdentifier{
-				Remote: "git@github.com:user/repo.git",
+				Remote: "ssh://git@github.com/user/repo.git",
 				Ref:    "mybranch",
 				Subdir: "mydir/mysubdir/",
 			},

util/gitutil/git_ref_test.go
@@ -67,6 +67,13 @@ func TestParseGitRef(t *testing.T) {
 			},
 		},
 		{
+			ref: "custom.xyz/moby/buildkit.git",
+			expected: &GitRef{
+				Remote:    "https://custom.xyz/moby/buildkit.git",
+				ShortName: "buildkit",
+			},
+		},
+		{
 			ref:      "https://github.com/moby/buildkit",
 			expected: nil,
 		},
@@ -78,23 +85,30 @@ func TestParseGitRef(t *testing.T) {
 			},
 		},
 		{
+			ref: "https://foo:bar@github.com/moby/buildkit.git",
+			expected: &GitRef{
+				Remote:    "https://foo:bar@github.com/moby/buildkit.git",
+				ShortName: "buildkit",
+			},
+		},
+		{
 			ref: "git@github.com:moby/buildkit",
 			expected: &GitRef{
-				Remote:    "git@github.com:moby/buildkit",
+				Remote:    "ssh://git@github.com/moby/buildkit",
 				ShortName: "buildkit",
 			},
 		},
 		{
 			ref: "git@github.com:moby/buildkit.git",
 			expected: &GitRef{
-				Remote:    "git@github.com:moby/buildkit.git",
+				Remote:    "ssh://git@github.com/moby/buildkit.git",
 				ShortName: "buildkit",
 			},
 		},
 		{
 			ref: "git@bitbucket.org:atlassianlabs/atlassian-docker.git",
 			expected: &GitRef{
-				Remote:    "git@bitbucket.org:atlassianlabs/atlassian-docker.git",
+				Remote:    "ssh://git@bitbucket.org/atlassianlabs/atlassian-docker.git",
 				ShortName: "atlassian-docker",
 			},
 		},

util/gitutil/git_url_test.go
@@ -1,0 +1,121 @@
+package gitutil
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseURL(t *testing.T) {
+	tests := []struct {
+		url      string
+		protocol string
+		host     string
+		fragment string
+		path     string
+		user     string
+		err      bool
+	}{
+		{
+			url:      "http://github.com/moby/buildkit",
+			protocol: HTTPProtocol,
+			host:     "github.com",
+			path:     "/moby/buildkit",
+		},
+		{
+			url:      "https://github.com/moby/buildkit",
+			protocol: HTTPSProtocol,
+			host:     "github.com",
+			path:     "/moby/buildkit",
+		},
+		{
+			url:      "http://github.com/moby/buildkit#v1.0.0",
+			protocol: HTTPProtocol,
+			host:     "github.com",
+			path:     "/moby/buildkit",
+			fragment: "v1.0.0",
+		},
+		{
+			url:      "http://foo:bar@github.com/moby/buildkit#v1.0.0",
+			protocol: HTTPProtocol,
+			host:     "github.com",
+			path:     "/moby/buildkit",
+			fragment: "v1.0.0",
+			user:     "foo:bar",
+		},
+		{
+			url:      "ssh://git@github.com/moby/buildkit.git",
+			protocol: SSHProtocol,
+			host:     "github.com",
+			path:     "/moby/buildkit.git",
+			user:     "git",
+		},
+		{
+			url:      "ssh://git@github.com:22/moby/buildkit.git",
+			protocol: SSHProtocol,
+			host:     "github.com:22",
+			path:     "/moby/buildkit.git",
+			user:     "git",
+		},
+		{
+			url:      "git@github.com:moby/buildkit.git",
+			protocol: SSHProtocol,
+			host:     "github.com",
+			path:     "/moby/buildkit.git",
+			user:     "git",
+		},
+		{
+			url:      "git@github.com:moby/buildkit.git#v1.0.0",
+			protocol: SSHProtocol,
+			host:     "github.com",
+			path:     "/moby/buildkit.git",
+			fragment: "v1.0.0",
+			user:     "git",
+		},
+		{
+			url:      "nonstandarduser@example.com:/srv/repos/weird/project.git",
+			protocol: SSHProtocol,
+			host:     "example.com",
+			path:     "/srv/repos/weird/project.git",
+			user:     "nonstandarduser",
+		},
+		{
+			url:      "ssh://root@subdomain.example.hostname:2222/root/my/really/weird/path/foo.git",
+			protocol: SSHProtocol,
+			host:     "subdomain.example.hostname:2222",
+			path:     "/root/my/really/weird/path/foo.git",
+			user:     "root",
+		},
+		{
+			url:      "git://host.xz:1234/path/to/repo.git",
+			protocol: GitProtocol,
+			host:     "host.xz:1234",
+			path:     "/path/to/repo.git",
+		},
+		{
+			url: "httpx://github.com/moby/buildkit",
+			err: true,
+		},
+		{
+			url:      "HTTP://github.com/moby/buildkit",
+			protocol: HTTPProtocol,
+			host:     "github.com",
+			path:     "/moby/buildkit",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.url, func(t *testing.T) {
+			remote, err := ParseURL(test.url)
+			if test.err {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, test.protocol, remote.Scheme)
+				require.Equal(t, test.host, remote.Host)
+				require.Equal(t, test.path, remote.Path)
+				require.Equal(t, test.fragment, remote.Fragment)
+				require.Equal(t, test.user, remote.User.String())
+			}
+		})
+	}
+}

util/gitutil/git_protocol.go
@@ -1,46 +1,0 @@
-package gitutil
-
-import (
-	"strings"
-
-	"github.com/moby/buildkit/util/sshutil"
-)
-
-const (
-	HTTPProtocol = iota + 1
-	HTTPSProtocol
-	SSHProtocol
-	GitProtocol
-	UnknownProtocol
-)
-
-// ParseProtocol parses a git URL and returns the remote url and protocol type
-func ParseProtocol(remote string) (string, int) {
-	prefixes := map[string]int{
-		"http://":  HTTPProtocol,
-		"https://": HTTPSProtocol,
-		"git://":   GitProtocol,
-		"ssh://":   SSHProtocol,
-	}
-	protocolType := UnknownProtocol
-	for prefix, potentialType := range prefixes {
-		if strings.HasPrefix(remote, prefix) {
-			remote = strings.TrimPrefix(remote, prefix)
-			protocolType = potentialType
-		}
-	}
-
-	if protocolType == UnknownProtocol && sshutil.IsImplicitSSHTransport(remote) {
-		protocolType = SSHProtocol
-	}
-
-	// remove name from ssh
-	if protocolType == SSHProtocol {
-		parts := strings.SplitN(remote, "@", 2)
-		if len(parts) == 2 {
-			remote = parts[1]
-		}
-	}
-
-	return remote, protocolType
-}

commit 9a51bb0ff1342272787040a345bceeccce606065

git: use custom giturl type to preserve original remote
This resolves a regression introduced in
50e75e3565b0e301a364d64000c4739e1abdc5f1. In this previous patch, I'd
incorrectly assumed that scp-like URLs can express a subset of
"standard"-URLs and so we can always safely convert them for
consistency. This isn't true - the URL "git@example.com:foo" should be
resolved to the home directory of the host, however, the converted URL
"ssh://git@example.com/foo" will be resolved to the root of the host.

To resolve this, we need to not perform this conversion. However, we
also need preserve the behaviour of firm distinction between SCP and
normal URL types (so as to keep proper port parsing).

To do this, we add a new GitURL type to the gitutil package. This new
type contains all useful fields shared in common between the standard
libraries url package and our custom scp-style url parsing package. This
keeps the previous property of a single clean interface to all GitURLs,
while also ensuring that we preserve the original URL to pass to the Git
CLI (making sure we strip fragments out, which are used as
buildkit-level metadata).

As a side-effect of this, the client-side calling code for parsing
git urls is simplified (so we don't have to do fragment wrangling at
every call point).

Signed-off-by: Justin Chadwell <me@jedevc.com>

client/llb/source.go
@@ -245,8 +245,7 @@ func Git(url, ref string, opts ...GitOption) State {
 		remote, err = gitutil.ParseURL(url)
 	}
 	if remote != nil {
-		remote.Fragment = ""
-		url = remote.String()
+		url = remote.Remote
 	}
 
 	var id string

source/git/identifier.go
@@ -31,11 +31,11 @@ func NewGitIdentifier(remoteURL string) (*GitIdentifier, error) {
 		return nil, err
 	}
 
-	repo := GitIdentifier{}
-	repo.Ref, repo.Subdir = gitutil.SplitGitFragment(u.Fragment)
-	u.Fragment = ""
-	repo.Remote = u.String()
-
+	repo := GitIdentifier{Remote: u.Remote}
+	if u.Fragment != nil {
+		repo.Ref = u.Fragment.Ref
+		repo.Subdir = u.Fragment.Subdir
+	}
 	if sd := path.Clean(repo.Subdir); sd == "/" || sd == "." {
 		repo.Subdir = ""
 	}

util/gitutil/git_ref.go
@@ -53,17 +53,17 @@ func ParseGitRef(ref string) (*GitRef, error) {
 	res := &GitRef{}
 
 	var (
-		remote *url.URL
+		remote *GitURL
 		err    error
 	)
 
 	if strings.HasPrefix(ref, "github.com/") {
 		res.IndistinguishableFromLocal = true // Deprecated
-		remote = &url.URL{
+		remote = fromURL(&url.URL{
 			Scheme: "https",
 			Host:   "github.com",
 			Path:   strings.TrimPrefix(ref, "github.com/"),
-		}
+		})
 	} else {
 		remote, err = ParseURL(ref)
 		if errors.Is(err, ErrUnknownProtocol) {
@@ -87,12 +87,12 @@ func ParseGitRef(ref string) (*GitRef, error) {
 		}
 	}
 
-	res.Commit, res.SubDir = SplitGitFragment(remote.Fragment)
-	remote.Fragment = ""
-
-	res.Remote = remote.String()
+	res.Remote = remote.Remote
 	if res.IndistinguishableFromLocal {
 		_, res.Remote, _ = strings.Cut(res.Remote, "://")
+	}
+	if remote.Fragment != nil {
+		res.Commit, res.SubDir = remote.Fragment.Ref, remote.Fragment.Subdir
 	}
 
 	repoSplitBySlash := strings.Split(res.Remote, "/")

util/gitutil/git_url.go
@@ -30,42 +30,94 @@ var supportedProtos = map[string]struct{}{
 
 var protoRegexp = regexp.MustCompile(`^[a-zA-Z0-9]+://`)
 
-// ParseURL parses a git URL and returns a parsed URL object.
+// URL is a custom URL type that points to a remote Git repository.
 //
-// ParseURL understands implicit ssh URLs such as "git@host:repo", and
-// returns the same response as if the URL were "ssh://git@host/repo".
-func ParseURL(remote string) (*url.URL, error) {
+// URLs can be parsed from both standard URLs (e.g.
+// "https://github.com/moby/buildkit.git"), as well as SCP-like URLs (e.g.
+// "git@github.com:moby/buildkit.git").
+//
+// See https://git-scm.com/book/en/v2/Git-on-the-Server-The-Protocols
+type GitURL struct {
+	// Scheme is the protocol over which the git repo can be accessed
+	Scheme string
+
+	// Host is the remote host that hosts the git repo
+	Host string
+	// Path is the path on the host to access the repo
+	Path string
+	// User is the username/password to access the host
+	User *url.Userinfo
+	// Fragment can contain additional metadata
+	Fragment *GitURLFragment
+
+	// Remote is a valid URL remote to pass into the Git CLI tooling (i.e.
+	// without the fragment metadata)
+	Remote string
+}
+
+// GitURLFragment is the buildkit-specific metadata extracted from the fragment
+// of a remote URL.
+type GitURLFragment struct {
+	// Ref is the git reference
+	Ref string
+	// Subdir is the sub-directory inside the git repository to use
+	Subdir string
+}
+
+// splitGitFragment splits a git URL fragment into its respective git
+// reference and subdirectory components.
+func splitGitFragment(fragment string) *GitURLFragment {
+	if fragment == "" {
+		return nil
+	}
+	ref, subdir, _ := strings.Cut(fragment, ":")
+	return &GitURLFragment{Ref: ref, Subdir: subdir}
+}
+
+// ParseURL parses a BuildKit-style Git URL (that may contain additional
+// fragment metadata) and returns a parsed GitURL object.
+func ParseURL(remote string) (*GitURL, error) {
 	if proto := protoRegexp.FindString(remote); proto != "" {
 		proto = strings.ToLower(strings.TrimSuffix(proto, "://"))
 		if _, ok := supportedProtos[proto]; !ok {
 			return nil, errors.Wrap(ErrInvalidProtocol, proto)
 		}
-
-		return url.Parse(remote)
+		url, err := url.Parse(remote)
+		if err != nil {
+			return nil, err
+		}
+		return fromURL(url), nil
 	}
 
-	if sshutil.IsImplicitSSHTransport(remote) {
-		remote, fragment, _ := strings.Cut(remote, "#")
-		remote, path, _ := strings.Cut(remote, ":")
-		user, host, _ := strings.Cut(remote, "@")
-		if !strings.HasPrefix(path, "/") {
-			path = "/" + path
-		}
-		return &url.URL{
-			Scheme:   SSHProtocol,
-			User:     url.User(user),
-			Host:     host,
-			Path:     path,
-			Fragment: fragment,
-		}, nil
+	if url, err := sshutil.ParseSCPStyleURL(remote); err == nil {
+		return fromSCPStyleURL(url), nil
 	}
 
 	return nil, ErrUnknownProtocol
 }
 
-// SplitGitFragments splits a git URL fragment into its respective git
-// reference and subdirectory components.
-func SplitGitFragment(fragment string) (ref string, subdir string) {
-	ref, subdir, _ = strings.Cut(fragment, ":")
-	return ref, subdir
+func fromURL(url *url.URL) *GitURL {
+	withoutFragment := *url
+	withoutFragment.Fragment = ""
+	return &GitURL{
+		Scheme:   url.Scheme,
+		User:     url.User,
+		Host:     url.Host,
+		Path:     url.Path,
+		Fragment: splitGitFragment(url.Fragment),
+		Remote:   withoutFragment.String(),
+	}
+}
+
+func fromSCPStyleURL(url *sshutil.SCPStyleURL) *GitURL {
+	withoutFragment := *url
+	withoutFragment.Fragment = ""
+	return &GitURL{
+		Scheme:   SSHProtocol,
+		User:     url.User,
+		Host:     url.Host,
+		Path:     url.Path,
+		Fragment: splitGitFragment(url.Fragment),
+		Remote:   withoutFragment.String(),
+	}
 }

source/git/identifier_test.go
@@ -27,7 +27,7 @@ func TestNewGitIdentifier(t *testing.T) {
 		{
 			url: "git@github.com:moby/buildkit.git",
 			expected: GitIdentifier{
-				Remote: "ssh://git@github.com/moby/buildkit.git",
+				Remote: "git@github.com:moby/buildkit.git",
 			},
 		},
 		{
@@ -75,13 +75,13 @@ func TestNewGitIdentifier(t *testing.T) {
 		{
 			url: "git@github.com:user/repo.git",
 			expected: GitIdentifier{
-				Remote: "ssh://git@github.com/user/repo.git",
+				Remote: "git@github.com:user/repo.git",
 			},
 		},
 		{
 			url: "git@github.com:user/repo.git#mybranch:mydir/mysubdir/",
 			expected: GitIdentifier{
-				Remote: "ssh://git@github.com/user/repo.git",
+				Remote: "git@github.com:user/repo.git",
 				Ref:    "mybranch",
 				Subdir: "mydir/mysubdir/",
 			},

util/gitutil/git_ref_test.go
@@ -94,21 +94,21 @@ func TestParseGitRef(t *testing.T) {
 		{
 			ref: "git@github.com:moby/buildkit",
 			expected: &GitRef{
-				Remote:    "ssh://git@github.com/moby/buildkit",
+				Remote:    "git@github.com:moby/buildkit",
 				ShortName: "buildkit",
 			},
 		},
 		{
 			ref: "git@github.com:moby/buildkit.git",
 			expected: &GitRef{
-				Remote:    "ssh://git@github.com/moby/buildkit.git",
+				Remote:    "git@github.com:moby/buildkit.git",
 				ShortName: "buildkit",
 			},
 		},
 		{
 			ref: "git@bitbucket.org:atlassianlabs/atlassian-docker.git",
 			expected: &GitRef{
-				Remote:    "ssh://git@bitbucket.org/atlassianlabs/atlassian-docker.git",
+				Remote:    "git@bitbucket.org:atlassianlabs/atlassian-docker.git",
 				ShortName: "atlassian-docker",
 			},
 		},

util/gitutil/git_url_test.go
@@ -1,6 +1,7 @@
 package gitutil
 
 import (
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -8,99 +9,147 @@ import (
 
 func TestParseURL(t *testing.T) {
 	tests := []struct {
-		url      string
-		protocol string
-		host     string
-		fragment string
-		path     string
-		user     string
-		err      bool
+		url    string
+		result GitURL
+		err    bool
 	}{
 		{
-			url:      "http://github.com/moby/buildkit",
-			protocol: HTTPProtocol,
-			host:     "github.com",
-			path:     "/moby/buildkit",
+			url: "http://github.com/moby/buildkit",
+			result: GitURL{
+				Scheme: HTTPProtocol,
+				Host:   "github.com",
+				Path:   "/moby/buildkit",
+			},
 		},
 		{
-			url:      "https://github.com/moby/buildkit",
-			protocol: HTTPSProtocol,
-			host:     "github.com",
-			path:     "/moby/buildkit",
+			url: "https://github.com/moby/buildkit",
+			result: GitURL{
+				Scheme: HTTPSProtocol,
+				Host:   "github.com",
+				Path:   "/moby/buildkit",
+			},
 		},
 		{
-			url:      "http://github.com/moby/buildkit#v1.0.0",
-			protocol: HTTPProtocol,
-			host:     "github.com",
-			path:     "/moby/buildkit",
-			fragment: "v1.0.0",
+			url: "http://github.com/moby/buildkit#v1.0.0",
+			result: GitURL{
+				Scheme:   HTTPProtocol,
+				Host:     "github.com",
+				Path:     "/moby/buildkit",
+				Fragment: &GitURLFragment{Ref: "v1.0.0"},
+			},
 		},
 		{
-			url:      "http://foo:bar@github.com/moby/buildkit#v1.0.0",
-			protocol: HTTPProtocol,
-			host:     "github.com",
-			path:     "/moby/buildkit",
-			fragment: "v1.0.0",
-			user:     "foo:bar",
+			url: "http://github.com/moby/buildkit#v1.0.0:subdir",
+			result: GitURL{
+				Scheme:   HTTPProtocol,
+				Host:     "github.com",
+				Path:     "/moby/buildkit",
+				Fragment: &GitURLFragment{Ref: "v1.0.0", Subdir: "subdir"},
+			},
 		},
 		{
-			url:      "ssh://git@github.com/moby/buildkit.git",
-			protocol: SSHProtocol,
-			host:     "github.com",
-			path:     "/moby/buildkit.git",
-			user:     "git",
+			url: "http://foo:bar@github.com/moby/buildkit#v1.0.0",
+			result: GitURL{
+				Scheme:   HTTPProtocol,
+				Host:     "github.com",
+				Path:     "/moby/buildkit",
+				Fragment: &GitURLFragment{Ref: "v1.0.0"},
+				User:     url.UserPassword("foo", "bar"),
+			},
 		},
 		{
-			url:      "ssh://git@github.com:22/moby/buildkit.git",
-			protocol: SSHProtocol,
-			host:     "github.com:22",
-			path:     "/moby/buildkit.git",
-			user:     "git",
+			url: "ssh://git@github.com/moby/buildkit.git",
+			result: GitURL{
+				Scheme: SSHProtocol,
+				Host:   "github.com",
+				Path:   "/moby/buildkit.git",
+				User:   url.User("git"),
+			},
 		},
 		{
-			url:      "git@github.com:moby/buildkit.git",
-			protocol: SSHProtocol,
-			host:     "github.com",
-			path:     "/moby/buildkit.git",
-			user:     "git",
+			url: "ssh://git@github.com:22/moby/buildkit.git",
+			result: GitURL{
+				Scheme: SSHProtocol,
+				Host:   "github.com:22",
+				Path:   "/moby/buildkit.git",
+				User:   url.User("git"),
+			},
 		},
 		{
-			url:      "git@github.com:moby/buildkit.git#v1.0.0",
-			protocol: SSHProtocol,
-			host:     "github.com",
-			path:     "/moby/buildkit.git",
-			fragment: "v1.0.0",
-			user:     "git",
+			url: "git@github.com:moby/buildkit.git",
+			result: GitURL{
+				Scheme: SSHProtocol,
+				Host:   "github.com",
+				Path:   "moby/buildkit.git",
+				User:   url.User("git"),
+			},
 		},
 		{
-			url:      "nonstandarduser@example.com:/srv/repos/weird/project.git",
-			protocol: SSHProtocol,
-			host:     "example.com",
-			path:     "/srv/repos/weird/project.git",
-			user:     "nonstandarduser",
+			url: "git@github.com:moby/buildkit.git#v1.0.0",
+			result: GitURL{
+				Scheme:   SSHProtocol,
+				Host:     "github.com",
+				Path:     "moby/buildkit.git",
+				Fragment: &GitURLFragment{Ref: "v1.0.0"},
+				User:     url.User("git"),
+			},
 		},
 		{
-			url:      "ssh://root@subdomain.example.hostname:2222/root/my/really/weird/path/foo.git",
-			protocol: SSHProtocol,
-			host:     "subdomain.example.hostname:2222",
-			path:     "/root/my/really/weird/path/foo.git",
-			user:     "root",
+			url: "git@github.com:moby/buildkit.git#v1.0.0:hack",
+			result: GitURL{
+				Scheme:   SSHProtocol,
+				Host:     "github.com",
+				Path:     "moby/buildkit.git",
+				Fragment: &GitURLFragment{Ref: "v1.0.0", Subdir: "hack"},
+				User:     url.User("git"),
+			},
 		},
 		{
-			url:      "git://host.xz:1234/path/to/repo.git",
-			protocol: GitProtocol,
-			host:     "host.xz:1234",
-			path:     "/path/to/repo.git",
+			url: "nonstandarduser@example.com:/srv/repos/weird/project.git",
+			result: GitURL{
+				Scheme: SSHProtocol,
+				Host:   "example.com",
+				Path:   "/srv/repos/weird/project.git",
+				User:   url.User("nonstandarduser"),
+			},
+		},
+		{
+			url: "ssh://root@subdomain.example.hostname:2222/root/my/really/weird/path/foo.git",
+			result: GitURL{
+				Scheme: SSHProtocol,
+				Host:   "subdomain.example.hostname:2222",
+				Path:   "/root/my/really/weird/path/foo.git",
+				User:   url.User("root"),
+			},
+		},
+		{
+			url: "git://host.xz:1234/path/to/repo.git",
+			result: GitURL{
+				Scheme: GitProtocol,
+				Host:   "host.xz:1234",
+				Path:   "/path/to/repo.git",
+			},
+		},
+		{
+			url: "ssh://someuser@192.168.0.123:456/~/repo-in-my-home-dir.git",
+			result: GitURL{
+				Scheme: SSHProtocol,
+				Host:   "192.168.0.123:456",
+				Path:   "/~/repo-in-my-home-dir.git",
+				User:   url.User("someuser"),
+			},
 		},
 		{
 			url: "httpx://github.com/moby/buildkit",
 			err: true,
 		},
 		{
-			url:      "HTTP://github.com/moby/buildkit",
-			protocol: HTTPProtocol,
-			host:     "github.com",
-			path:     "/moby/buildkit",
+			url: "HTTP://github.com/moby/buildkit",
+			result: GitURL{
+				Scheme: HTTPProtocol,
+				Host:   "github.com",
+				Path:   "/moby/buildkit",
+			},
 		},
 	}
 	for _, test := range tests {
@@ -110,11 +159,11 @@ func TestParseURL(t *testing.T) {
 				require.Error(t, err)
 			} else {
 				require.NoError(t, err)
-				require.Equal(t, test.protocol, remote.Scheme)
-				require.Equal(t, test.host, remote.Host)
-				require.Equal(t, test.path, remote.Path)
-				require.Equal(t, test.fragment, remote.Fragment)
-				require.Equal(t, test.user, remote.User.String())
+				require.Equal(t, test.result.Scheme, remote.Scheme)
+				require.Equal(t, test.result.Host, remote.Host)
+				require.Equal(t, test.result.Path, remote.Path)
+				require.Equal(t, test.result.Fragment, remote.Fragment)
+				require.Equal(t, test.result.User.String(), remote.User.String())
 			}
 		})
 	}