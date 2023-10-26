5ecfec1039959e8b742494eec8f7b5fd8202df5a
Milas Bowman <milas.bowman@docker.com>
2023-10-26 17:09:20 -0400

    ci(deps): upgrade OpenTelemetry libraries to v1.19.0 / v0.45.0
    Core OTel libs are now at v1.19.0, while contrib is at v0.45.0.
    The protocol has also reached 1.0.0.
    
    Note that Jaeger exporter is frozen at v1.17.0, which is the
    last version it was supported by OpenTelemetry. BuildKit should
    likely begin the process of deprecating it as well, since OTLP
    is now the recommended solution (by Jaeger themselves), but that
    is not handled here.
    
    Signed-off-by: Milas Bowman <milas.bowman@docker.com>

--- util/tracing/detect/detect.go
+++ util/tracing/detect/detect.go
@@ -12,7 +12,7 @@ import (
 	"github.com/pkg/errors"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
