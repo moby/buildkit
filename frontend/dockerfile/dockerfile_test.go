a2033b66313ef2589a9108621bf78cc5cf68e685
Billy Owire <billyowire@microsoft.com>
2024-07-12 18:47:58 +0300

    Integration tests: frontend/dockerfile/

--- frontend/dockerfile/dockerfile_test.go
+++ frontend/dockerfile/dockerfile_test.go
@@ -913,17 +913,25 @@ RUN e="300:400"; p="/file"                         ; a=` + "`" + `stat -c "%u:%g
 }
 
 func testCopyWildcardCache(t *testing.T, sb integration.Sandbox) {
-	integration.SkipOnPlatform(t, "windows")
+	//integration.SkipOnPlatform(t, "windows")
 	f := getFrontend(t, sb)
 
-	dockerfile := []byte(`
+	dockerfile := []byte(integration.UnixOrWindows([2]string{`
 FROM busybox AS base
 COPY foo* files/
 RUN cat /dev/urandom | head -c 100 | sha256sum > unique
 COPY bar files/
 FROM scratch
 COPY --from=base unique /
-`)
+`, `
+FROM nanoserver AS base
+USER ContainerAdministrator
+RUN  echo foo1 content > C:\\foo1.txt && echo foo2 content > C:\\foo2.txt
+
+FROM nanoserver
+USER ContainerAdministrator
+COPY --from=base C:\\foo1.txt C:\\files
+`}))
 
 	dir := integration.Tmpdir(
 		t,
@@ -1004,13 +1012,14 @@ COPY --from=base unique /
 }
 
 func testEmptyWildcard(t *testing.T, sb integration.Sandbox) {
-	integration.SkipOnPlatform(t, "windows")
 	f := getFrontend(t, sb)
 
-	dockerfile := []byte(`
-FROM scratch
+	dockerfile := []byte(integration.UnixOrWindows([2]string{
+		`FROM scratch
 COPY foo nomatch* /
-`)
+`,
+		`FROM nanoserver
+COPY foo nomatch* /`}))
 
 	dir := integration.Tmpdir(
 		t,
@@ -1044,16 +1053,21 @@ COPY foo nomatch* /
 }
 
 func testWorkdirUser(t *testing.T, sb integration.Sandbox) {
-	integration.SkipOnPlatform(t, "windows")
 	f := getFrontend(t, sb)
 
-	dockerfile := []byte(`
-FROM busybox
+	dockerfile := []byte(integration.UnixOrWindows([2]string{
+		`FROM busybox
 RUN adduser -D user
 USER user
 WORKDIR /mydir
 RUN [ "$(stat -c "%U %G" /mydir)" == "user user" ]
-`)
+`,
+		`nanoserver
+USER ContainerAdministrator
+RUN mkdir \mydir
+WORKDIR \mydir
+RUN icacls C:\\mydir | findstr "Administrators">nul || exit /b 1
+`}))
 
 	dir := integration.Tmpdir(
 		t,
@@ -1074,19 +1088,31 @@ RUN [ "$(stat -c "%U %G" /mydir)" == "user user" ]
 }
 
 func testOutOfOrderStage(t *testing.T, sb integration.Sandbox) {
-	integration.SkipOnPlatform(t, "windows")
 	f := getFrontend(t, sb)
 
+	dockerfile := integration.UnixOrWindows([2]string{
+		`
+	FROM busybox AS target
+	COPY --from=build %s /out
+	
+	FROM alpine AS build
+	COPY /Dockerfile /d2
+	
+	FROM target
+	`,
+		`
+	FROM nanoserver AS target
+	COPY --from=build %s /out
+
+	FROM nanoserver AS build
+	COPY /Dockerfile /d2
+
+	FROM target
+	`})
+
 	for _, src := range []string{"/", "/d2"} {
-		dockerfile := []byte(fmt.Sprintf(`
-FROM busybox AS target
-COPY --from=build %s /out
 
-FROM alpine AS build
-COPY /Dockerfile /d2
-
-FROM target
-`, src))
+		dockerfile := []byte(fmt.Sprintf(dockerfile, src))
 
 		dir := integration.Tmpdir(
 			t,
@@ -1141,16 +1167,22 @@ COPY --from=base Dockerfile .
 }
 
 func testWorkdirExists(t *testing.T, sb integration.Sandbox) {
-	integration.SkipOnPlatform(t, "windows")
 	f := getFrontend(t, sb)
 
-	dockerfile := []byte(`
+	dockerfile := []byte(integration.UnixOrWindows([2]string{`
 FROM busybox
 RUN adduser -D user
 RUN mkdir /mydir && chown user:user /mydir
 WORKDIR /mydir
 RUN [ "$(stat -c "%U %G" /mydir)" == "user user" ]
-`)
+`, `
+FROM nanoserver
+USER ContainerAdministrator
+RUN mkdir \mydir
+RUN net user testuser Password!2345!@# /add /y
+RUN icacls \mydir /grant testuser:F
+WORKDIR /mydir
+RUN (icacls \mydir | findstr "testuser" >nul) || exit /b 1`}))
 
 	dir := integration.Tmpdir(
 		t,
