8f51e01ea52ee99a2e5461f740b00773caa0797b
Tonis Tiigi <tonistiigi@gmail.com>
2024-08-14 13:24:52 +0300

    update to go1.23
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- examples/nested-llb/main.go
+++ examples/nested-llb/main.go
@@ -32,7 +32,7 @@ func main() {
 }
 
 func goBuildBase() llb.State {
-	goAlpine := llb.Image("docker.io/library/golang:1.22-alpine")
+	goAlpine := llb.Image("docker.io/library/golang:3-alpine")
 	return goAlpine.
 		AddEnv("PATH", "/usr/local/go/bin:"+system.DefaultPathEnvUnix).
 		AddEnv("GOPATH", "/go").
