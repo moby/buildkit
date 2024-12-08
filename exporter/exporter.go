a0f00add8586035aae1387c5f9c8f0fe39c59a8d
a-palchikov <deemok@gmail.com>
2024-12-08 15:59:20 +0100

    The initial work on supporting multiple concurrent exporters was unfinished in the sense that exporter responses (which are abstracted as a map of key/value pairs) are combined into a single map at the end of the export: https://github.com/moby/buildkit/blob/55a7483b0564a7ad5b2ce5e62512789dce327bca/solver/llbsolver/solver.go#L808-L809
    In order to provide the correct exporter response, each response
    (currently at least each container image exporter) needs to be dedicated
    to the exporter instance.
    
    To achieve this, assign and propagate exporter IDs from the client and
    have corresponding exporters annotate their responses with the
    respective ID so the client can order outputs per exporter instance.
    
    Fixes https://github.com/moby/buildkit/issues/5556.
    
    Output descriptors in container image exporters with backwards-compatible fallbacks. Move the exporter set up to a public helper API so that users that custom-initialize the session can benefit from being able to configure exporters correctly.
    Remove ids from the exporter implementation further shifting the burden of managing exporter identities to the control/client so it can be unified and kept an implementation detail as much as possible. Implement support for multiple exporter responses in APIs.
    
    Signed-off-by: a-palchikov <deemok@gmail.com>

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -2,12 +2,15 @@ package exporter
 
 import (
 	"context"
+	"io"
 
 	"github.com/moby/buildkit/cache"
 	"github.com/moby/buildkit/exporter/containerimage/exptypes"
+	"github.com/moby/buildkit/session"
 	"github.com/moby/buildkit/solver/result"
 	"github.com/moby/buildkit/util/compression"
 	ocispecs "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/tonistiigi/fsutil"
 )
 
 type Source = result.Result[cache.ImmutableRef]
@@ -15,16 +18,20 @@ type Source = result.Result[cache.ImmutableRef]
 type Attestation = result.Attestation[cache.ImmutableRef]
 
 type Exporter interface {
-	Resolve(context.Context, int, map[string]string) (ExporterInstance, error)
+	Resolve(context.Context, map[string]string) (ExporterInstance, error)
 }
 
 type ExporterInstance interface {
-	ID() int
 	Name() string
 	Config() *Config
 	Type() string
 	Attrs() map[string]string
-	Export(ctx context.Context, src *Source, inlineCache exptypes.InlineCache, sessionID string) (map[string]string, DescriptorReference, error)
+	Export(ctx context.Context, src *Source, inlineCache exptypes.InlineCache, sessionID string, apis ExporterAPIs) (map[string]string, DescriptorReference, error)
+}
+
+type ExporterAPIs struct {
+	CopyFileWriter func(_ context.Context, md map[string]string, c session.Caller) (io.WriteCloser, error)
+	CopyToCaller   func(_ context.Context, fs fsutil.FS, c session.Caller, progress func(int, bool)) error
 }
 
 type DescriptorReference interface {
