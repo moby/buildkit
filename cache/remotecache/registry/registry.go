c097f433786f4964db0545d45d7b6fe969f6311a
liulanzheng <lanzheng.liulz@alibaba-inc.com>
2025-01-17 11:20:37 +0800

    Support for building overlaybd images
    Signed-off-by: liulanzheng <lanzheng.liulz@alibaba-inc.com>

--- cache/remotecache/registry/registry.go
+++ cache/remotecache/registry/registry.go
@@ -8,6 +8,8 @@ import (
 	"github.com/containerd/containerd/v2/core/content"
 	"github.com/containerd/containerd/v2/core/remotes/docker"
 	"github.com/containerd/containerd/v2/core/snapshots"
+	"github.com/containerd/containerd/v2/pkg/snapshotters"
+
 	"github.com/distribution/reference"
 	"github.com/moby/buildkit/cache/remotecache"
 	"github.com/moby/buildkit/session"
@@ -165,6 +167,7 @@ func (dsl *withDistributionSourceLabel) SnapshotLabels(descs []ocispecs.Descript
 		labels = make(map[string]string)
 	}
 	maps.Copy(labels, estargz.SnapshotLabels(dsl.ref, descs, index))
+	labels[snapshotters.TargetRefLabel] = dsl.ref
 	return labels
 }
 
