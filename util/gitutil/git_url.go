75e248d497ea18e27c81df2bf9e74fb6cd5ba5d9
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2025-05-15 18:34:34 +0900

    git url: support query form
    Fix issue 4905, but the syntax differs from the original proposal.
    
    The document will be added to
    https://github.com/docker/docs/blob/main/content/manuals/build/concepts/context.md#url-fragments
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- util/gitutil/git_url.go
+++ util/gitutil/git_url.go
@@ -56,7 +56,7 @@ type GitURL struct {
 }
 
 // GitURLOpts is the buildkit-specific metadata extracted from the fragment
-// of a remote URL.
+// or the query of a remote URL.
 type GitURLOpts struct {
 	// Ref is the git reference
 	Ref string
@@ -66,12 +66,63 @@ type GitURLOpts struct {
 
 // parseOpts splits a git URL fragment into its respective git
 // reference and subdirectory components.
-func parseOpts(fragment string) *GitURLOpts {
-	if fragment == "" {
-		return nil
+func parseOpts(fragment string, query url.Values) (*GitURLOpts, error) {
+	if fragment == "" && len(query) == 0 {
+		return nil, nil
 	}
-	ref, subdir, _ := strings.Cut(fragment, ":")
-	return &GitURLOpts{Ref: ref, Subdir: subdir}
+	opts := &GitURLOpts{}
+	if fragment != "" {
+		opts.Ref, opts.Subdir, _ = strings.Cut(fragment, ":")
+	}
+	var tag, branch string
+	for k, v := range query {
+		switch len(v) {
+		case 0:
+			return nil, errors.Errorf("query %q has no value", k)
+		case 1:
+			if v[0] == "" {
+				return nil, errors.Errorf("query %q has no value", k)
+			}
+			// NOP
+		default:
+			return nil, errors.Errorf("query %q has multiple values", k)
+		}
+		switch k {
+		case "ref":
+			if opts.Ref != "" && opts.Ref != v[0] {
+				return nil, errors.Errorf("ref conflicts: %q vs %q", opts.Ref, v[0])
+			}
+			opts.Ref = v[0]
+		case "tag":
+			tag = v[0]
+		case "branch":
+			branch = v[0]
+		case "subdir":
+			if opts.Subdir != "" && opts.Subdir != v[0] {
+				return nil, errors.Errorf("subdir conflicts: %q vs %q", opts.Subdir, v[0])
+			}
+			opts.Subdir = v[0]
+		default:
+			return nil, errors.Errorf("unexpected query %q", k)
+		}
+	}
+	if tag != "" {
+		if opts.Ref != "" {
+			return nil, errors.New("tag conflicts with ref")
+		}
+		opts.Ref = "refs/tags/" + tag
+	}
+	if branch != "" {
+		if tag != "" {
+			// TODO: consider allowing this, when the tag actually exists on the branch
+			return nil, errors.New("branch conflicts with tag")
+		}
+		if opts.Ref != "" {
+			return nil, errors.New("branch conflicts with ref")
+		}
+		opts.Ref = "refs/heads/" + branch
+	}
+	return opts, nil
 }
 
 // ParseURL parses a BuildKit-style Git URL (that may contain additional
@@ -86,11 +137,11 @@ func ParseURL(remote string) (*GitURL, error) {
 		if err != nil {
 			return nil, err
 		}
-		return fromURL(url), nil
+		return fromURL(url)
 	}
 
 	if url, err := sshutil.ParseSCPStyleURL(remote); err == nil {
-		return fromSCPStyleURL(url), nil
+		return fromSCPStyleURL(url)
 	}
 
 	return nil, ErrUnknownProtocol
@@ -105,28 +156,38 @@ func IsGitTransport(remote string) bool {
 	return sshutil.IsImplicitSSHTransport(remote)
 }
 
-func fromURL(url *url.URL) *GitURL {
+func fromURL(url *url.URL) (*GitURL, error) {
 	withoutOpts := *url
 	withoutOpts.Fragment = ""
+	withoutOpts.RawQuery = ""
+	opts, err := parseOpts(url.Fragment, url.Query())
+	if err != nil {
+		return nil, err
+	}
 	return &GitURL{
 		Scheme: url.Scheme,
 		User:   url.User,
 		Host:   url.Host,
 		Path:   url.Path,
-		Opts:   parseOpts(url.Fragment),
+		Opts:   opts,
 		Remote: withoutOpts.String(),
-	}
+	}, nil
 }
 
-func fromSCPStyleURL(url *sshutil.SCPStyleURL) *GitURL {
+func fromSCPStyleURL(url *sshutil.SCPStyleURL) (*GitURL, error) {
 	withoutOpts := *url
 	withoutOpts.Fragment = ""
+	withoutOpts.Query = nil
+	opts, err := parseOpts(url.Fragment, url.Query)
+	if err != nil {
+		return nil, err
+	}
 	return &GitURL{
 		Scheme: SSHProtocol,
 		User:   url.User,
 		Host:   url.Host,
 		Path:   url.Path,
-		Opts:   parseOpts(url.Fragment),
+		Opts:   opts,
 		Remote: withoutOpts.String(),
-	}
+	}, nil
 }
