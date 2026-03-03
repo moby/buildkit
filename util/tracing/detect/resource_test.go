bfaa12ba71c4b1e13f21c0db28bae85af9dc746c
Sebastiaan van Stijn <github@gone.nl>
2026-03-03 12:39:45 +0100

    util/tracing: remove semconv dependency
    Replace semconv imports with local constants for semconv keys.
    
    These attributes are marked Stable in the OpenTelemetry Semantic
    Conventions and are not expected to change across semver releases:
    
    - https://opentelemetry.io/docs/specs/semconv/
    - https://opentelemetry.io/docs/specs/otel/versioning-and-stability/#semantic-conventions-stability
    
    This avoids pulling in versioned semconv packages solely for stable
    attribute keys and prevents duplicate semconv versions in the module
    graph.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- util/tracing/detect/resource_test.go
+++ util/tracing/detect/resource_test.go
@@ -22,7 +22,16 @@ func TestResource(t *testing.T) {
 
 	// Should not have an empty schema url. Only happens when
 	// there is a schema conflict.
-	require.NotEqual(t, "", res.SchemaURL())
+	require.NotEmpty(t, res.SchemaURL())
+
+	var found bool
+	for iter := res.Iter(); iter.Next(); {
+		if iter.Attribute().Key == serviceNameKey {
+			found = true
+			break
+		}
+	}
+	require.True(t, found, "expected to find service name attribute")
 
 	// No error should have been invoked.
 	require.NoError(t, resourceErr)
