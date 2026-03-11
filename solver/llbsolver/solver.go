6bcae31d185d7138af7e5e0e489697734f5de0e9
Tonis Tiigi <tonistiigi@gmail.com>
2026-03-10 22:50:32 -0700

    refactor: extract entitlements logic from llbsolver/solver.go
    Move keyEntitlements constant, supportedEntitlements, and
    loadEntitlements into a dedicated entitlements.go file.
    No functional changes.
    
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- solver/llbsolver/solver.go
+++ solver/llbsolver/solver.go
@@ -44,10 +44,6 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-const (
-	keyEntitlements = "llb.entitlements"
-)
-
 type ExporterRequest struct {
 	Exporters             []exporter.ExporterInstance
 	CacheExporters        []RemoteCacheExporter
@@ -785,42 +781,4 @@ func notifyStarted(ctx context.Context, v *client.Vertex) func(err error) {
 		}
 		pw.Write(id, *v)
 	}
-}
-
-func supportedEntitlements(ents []string) []entitlements.Entitlement {
-	out := []entitlements.Entitlement{} // nil means no filter
-	for _, e := range ents {
-		if e == string(entitlements.EntitlementNetworkHost) {
-			out = append(out, entitlements.EntitlementNetworkHost)
-		}
-		if e == string(entitlements.EntitlementSecurityInsecure) {
-			out = append(out, entitlements.EntitlementSecurityInsecure)
-		}
-		if e == string(entitlements.EntitlementDevice) {
-			out = append(out, entitlements.EntitlementDevice)
-		}
-	}
-	return out
-}
-
-func loadEntitlements(b solver.Builder) (entitlements.Set, error) {
-	var ent entitlements.Set = map[entitlements.Entitlement]entitlements.EntitlementsConfig{}
-	err := b.EachValue(context.TODO(), keyEntitlements, func(v any) error {
-		set, ok := v.(entitlements.Set)
-		if !ok {
-			return errors.Errorf("invalid entitlements %T", v)
-		}
-		for k, v := range set {
-			if prev, ok := ent[k]; ok && prev != nil {
-				prev.Merge(v)
-			} else {
-				ent[k] = v
-			}
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return ent, nil
 }
