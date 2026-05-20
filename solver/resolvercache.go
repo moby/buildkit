d6d7db53ebe9c6e677ad18240613f27576ed8a5d
Bryce Gibson <bgibson@limepoint.com>
2026-05-20 15:12:25 +1000

    Fix sigsegv by ensuring a func is returned in failure case.
    Signed-off-by: Bryce Gibson <bgibson@limepoint.com>

--- solver/resolvercache.go
+++ solver/resolvercache.go
@@ -61,7 +61,7 @@ func (r *resolverCache) Lock(key any) (values []any, release func(any) error, er
 	defer r.mu.Unlock()
 	e2, ok := r.locks[key]
 	if !ok {
-		return nil, nil, nil // key deleted
+		return nil, func(any) error { return nil }, nil // key deleted
 	}
 	values = slices.Clone(e2.values)
 	if e2.locked {
