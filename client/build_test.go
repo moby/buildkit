35daa44e972d4697c0bcbab7ac954b1b2c7963a2
Sebastiaan van Stijn <github@gone.nl>
2026-06-17 11:09:14 +0200

    client: remove use of deprecated proxy.Director (staticcheck)
        client/build_test.go:2363:14: SA1019: proxy.Director has been deprecated since Go 1.26 and an alternative has been available since Go 1.20: Use Rewrite instead. (staticcheck)
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- client/build_test.go
+++ client/build_test.go
@@ -2360,10 +2360,9 @@ func testClientGatewayCanceledCredentialsCallbackReturns(t *testing.T, sb integr
 	require.NoError(t, err)
 
 	proxy := httputil.NewSingleHostReverseProxy(target)
-	director := proxy.Director
-	proxy.Director = func(req *http.Request) {
-		director(req)
-		req.Host = target.Host
+	proxy.Rewrite = func(pr *httputil.ProxyRequest) {
+		pr.SetURL(target)
+		pr.Out.Host = target.Host
 	}
 
 	proxyServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
